73410e44a04d5ac526c77e7069bb40e18ba940d4
Jakub Szczęsny <jszczesny@sii.pl>
2022-04-07 16:32:54 +0200

    Add reading from txt file

--- Image_Converter_to_TXT/16x16.c
+++ Image_Converter_to_TXT/16x16.c
@@ -13,21 +13,20 @@ int main(int argc, char **argv)
         getchar();
         return 0;
     }
-    mkdir("output");
-    
-    for (int i = 1; i < argc; i++)
-    {
-        printf("Loaded file:\n");
-        char* tmp = strrchr(argv[i], '\\');
-        char tmp2[40];
-        strncpy(tmp2, &tmp[1], (size_t)(strlen(tmp)-5));
-        tmp2[strlen(tmp)-5] = '\0';
-        printf("%s\n",  tmp2);
 
-        printf("Convert FILE %s to TXT format\r\n",  tmp2);
+    printf("Loaded file:\n");
+    char* tmp = strrchr(argv[1], '\\');
+    char tmp2[40];
+    strncpy(tmp2, &tmp[1], (size_t)(strlen(tmp)-7));
+    tmp2[strlen(tmp)-7] = '\0';
+
+    mkdir(tmp2);
+    printf("Convert FILES %s to TXT format\r\n",  tmp2);
+    for (int i = 1; i < argc; i++)
+    {        
         FILE *file;
         char file_name[40];
-        sprintf(file_name, "./output/%s.txt", tmp2);
+        sprintf(file_name, "./%s/%d.txt", tmp2, i - 1);
         printf("%s\n", file_name);
         file = fopen(file_name, "w+");
 
--- Image_Converter_to_TXT/read_txt.c
+++ Image_Converter_to_TXT/read_txt.c
@@ -1,11 +1,71 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 int main(void)
 {
-    char tmp2[] = "dupa";
-    char file_name[40];
-    sprintf(file_name, "./%s.txt", tmp2);
-    printf("%s\n", file_name);
+    FILE *file;
+    if((file = fopen("./walking_anim/0.txt", "r")) == NULL){
+        printf("Error! opening file\n");
+    }
+    // int num = 0;
+    // while(!feof(file)){
+    //     num++;
+    //     fgetc(file);
+    // // }
+    // fseek(file, 0, SEEK_SET);
+    // // char* tmp = strrchr(argv[1], '\\')
+
+    // printf("n = %d\n", num);
+    // char string[num];
+
+    // fgets(string, num, file);
+    // printf("Value of n = %s", string);
+
+    int array[1][256][3] = {0};
+    int i = 0;
+    for (i = 0; i < 256; i++){
+        fscanf(file, "%d,", &array[0][i][0]);
+        fscanf(file, "%d,", &array[0][i][1]);
+        fscanf(file, "%d,", &array[0][i][2]);
+    }
+
+    // uint8_t array[1][256][3] = {0};
+    // int i = 0;
+    // int j = 0;
+    // int curr = 0;
+    // rewind(file);
+    // while(!feof(file)){
+    //     char string[4] = {0};
+    //     // fgets(string, 5, file);
+    //     fread(string, 1, 4, file);
+    //     printf("string = %s \n", string);
+        
+    //     char *token = strtok(string, ",");
+    //     curr += strlen(token)+1;
+    //     fseek(file, curr, SEEK_SET);
+        
+    //     array[1][i][j] = atoi(token);
+    //     printf("array[1][%d][%d] = %d \n", i, j, array[1][i][j]);
+    //     j++;
+    //     if(2 < j){
+    //         j = 0;
+    //         i++;  
+    //     }   
+    // }
+    fclose(file);
+
+
+    printf("Read data:\n");
+    for (size_t x = 0; x < 256; x++)
+    {
+        printf("{%d, %d, %d}, ", array[0][x][0], array[0][x][1], array[0][x][2]);
+        if(x != 0 && (x+1)%16==0)
+            printf("\n");
+    }
+    
+
     getchar();
     return 0;
 }
